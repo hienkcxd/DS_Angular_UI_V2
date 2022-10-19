358bc853ceecf7e0ae1727f8d3e4f7ac408c2dac
Hien <hien03kc@gmail.com>
2022-10-19 15:53:35 +0700

    Update cau truc project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './module/layout/header/header.component';
+import { FooterComponent } from './module/layout/footer/footer.component';
+import { AdminSidebarComponent } from './module/layout/admin-sidebar/admin-sidebar.component';
+import { UserSidebarComponent } from './module/layout/user-sidebar/user-sidebar.component';
+import {DataTablesModule} from "angular-datatables";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AdminSidebarComponent,
+    UserSidebarComponent
   ],
   imports: [
     BrowserModule,
+    DataTablesModule,
     AppRoutingModule
   ],
   providers: [],
