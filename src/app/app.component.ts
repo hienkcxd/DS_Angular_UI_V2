f02af5a617a81ea0ef8703cdfce178684e2f7c9f
Hien <hien03kc@gmail.com>
2022-10-19 15:33:29 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'DS_Angular_UI_V2';
+}
