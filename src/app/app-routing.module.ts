964925576a27543cae38fdff5ed508d4592192eb
Hien <hien03kc@gmail.com>
2022-10-19 16:05:33 +0700

    Update layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./module/home/home/home.component";
+import {ProfileComponent} from "./module/profile/profile/profile.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path:"", component: LoginComponent},
+  // {path:"404", component: Errors404Component},
+  // {path:"500", component: Errors500Component},
+  {path:"home", component: HomeComponent},
+  {path:"user-detail", component: ProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
