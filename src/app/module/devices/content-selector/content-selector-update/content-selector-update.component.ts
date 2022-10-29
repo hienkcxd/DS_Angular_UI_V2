935a11d44d510d47525911ac5d593dfdb70c75a1
at1220 <tuan22ee@gmail.com>
2022-10-29 15:32:20 +0700

    tuan-devices-add-new-component

---
+++ src/app/module/devices/content-selector/content-selector-update/content-selector-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-selector-update',
+  templateUrl: './content-selector-update.component.html',
+  styleUrls: ['./content-selector-update.component.scss']
+})
+export class ContentSelectorUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
