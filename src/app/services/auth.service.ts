6a1d5453680bf3a972e539aca94ba9665cb0369c
hienkcxd <70784911+hienkcxd@users.noreply.github.com>
2022-12-04 16:24:20 +0700

    update login form and post

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {User} from "../model/user";
-import {Observable} from "rxjs";
+import {catchError, map, observable, Observable} from "rxjs";
+import * as Domain from "domain";
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,23 @@ import {Observable} from "rxjs";
 export class AuthService {
 
   constructor(private httpClient: HttpClient) { }
-  private baseUrl_Login="http://localhost:8080/api/login";
+  private baseUrl_Login = "http://localhost:8080";
+
+
   loginUser(user : User):Observable<object>{
     console.log("auth", user);
+
+    let httpOptions = {
+      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    };
+    let body = new URLSearchParams();
+    body.set('username', user.username);
+    body.set('password', user.password);
+
     const params = new HttpHeaders()
       .set('username', user.username)
       .set('password', user.password);
-    return this.httpClient.post(this.baseUrl_Login, {'username':user.username, 'password':user.password});
+
+    return this.httpClient.post(`${this.baseUrl_Login}/api/login`, body.toString(), httpOptions);
   }
 }
