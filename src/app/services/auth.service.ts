c2ed3e08ac0935dd97524424d04b56c22a91f81c
Hien <hien03kc@gmail.com>
2022-12-05 09:15:15 +0700

    updae login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {User} from "../model/user";
 import {catchError, map, observable, Observable} from "rxjs";
 import * as Domain from "domain";
+import {data} from "jquery";
 
 @Injectable({
   providedIn: 'root'
@@ -12,21 +13,23 @@ export class AuthService {
   constructor(private httpClient: HttpClient) { }
   private baseUrl_Login = "http://localhost:8080";
 
-
   loginUser(user : User):Observable<object>{
-    console.log("auth", user);
-
     let httpOptions = {
-      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+      headers: new HttpHeaders().set('Content-Type','application/x-www-form-urlencoded',),
+      responseType: 'text' as 'json'
     };
     let body = new URLSearchParams();
     body.set('username', user.username);
     body.set('password', user.password);
-
     const params = new HttpHeaders()
       .set('username', user.username)
       .set('password', user.password);
 
-    return this.httpClient.post(`${this.baseUrl_Login}/api/login`, body.toString(), httpOptions);
+    return this.httpClient.post(`${this.baseUrl_Login}/api/login`, body.toString(), httpOptions)
+      .subscribe(data=>{
+        localStorage.setItem('token',data.toString());
+      });
   }
+
+
 }
