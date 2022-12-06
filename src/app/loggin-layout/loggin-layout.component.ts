74bf2d5ce3cf102d810049a556e7fcc78b82ec8a
hienkcxd <70784911+hienkcxd@users.noreply.github.com>
2022-12-06 17:09:16 +0700

    update admin and user

---
+++ src/app/loggin-layout/loggin-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loggin-layout',
+  templateUrl: './loggin-layout.component.html',
+  styleUrls: ['./loggin-layout.component.scss']
+})
+export class LogginLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
